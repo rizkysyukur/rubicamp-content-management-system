9e8d5c6804bcd4de5618116f7b560049f8de2abc
saefulrizky <saefulrizky2@gmail>
2018-09-03 13:18:17 +0700

    Add api for register is done

--- server/test/index.js
+++ server/test/index.js
@@ -29,33 +29,65 @@ describe('note', function(){
     .post('/api/users/register')
     .send({
       email: "rizkysyukur@gmail.com",
-      password: "1234"
-    })
-    .end(function(err, res){
-      res.shoud.have.status(200);
+      password: "1234",
+      retypepassword: "1234"
+    }).end(function(err, res){
+      res.should.have.status(200);
       res.should.be.json;
       res.body.should.be.a('object');
-      res.body.should.have.property('data.email');
+      res.body.should.have.property('data');
       res.body.should.have.property('token');
+      res.body.data.should.be.a('object');
+      res.body.token.should.be.a('string');
+      res.body.data.email.should.equal("rizkysyukur@gmail.com");
       done();
     })
   })
 
-  it("Seharusnya menampilkan email dan token dengan metode POST", function(done){
+  it("Seharusnya sistem mengecek lalu valid dan mengembalikan email dan kode token dengan metode POST", function(done){
     chai.request(server)
-    .post('/api/users/register')
+    .post('/api/users/login')
     .send({
-      email: "rizkysyukur@gmail.com",
-      password: "1234"
-    })
-    .end(function(err, res){
-      res.body.should.have.status(200);
+      email: "anandareftina@gmail.com",
+      password: "1234",
+    }).end(function(err, res){
+      res.should.have.status(200);
       res.should.be.json;
       res.body.should.be.a('object');
-      res.body.should.have.property('email');
+      res.body.should.have.property('data');
       res.body.should.have.property('token');
+      res.body.data.should.be.a('object');
+      res.body.token.should.be.a('string');
+      res.body.data.email.should.equal("anandareftina@gmail.com");
       done();
     })
   })
 
+  it("Seharusnya sistem memverifikasi token dan mengembalikan hasil verifikasi berupa false dengan metode POST", function(done){
+    chai.request(server)
+    .post('/api/users/check')
+    .send({
+      token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFuYW5kYXJlZnRpbmFAZ21haWwuY29uIiwicGFzc3dvcmQiOiIxMjM0IiwiaWF0IjoxNTM1OTU1MDAxLCJleHAiOjE1MzYwNDE0MDF9.jar4pawlkkAQln-Wn_8UmUWzUoppK_EiI4zLSntkf6Y",
+    }).end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.have.property('valid');
+      res.body.valid.should.equal(false);
+      done();
+    })
+  })
+
+  it("Seharusnya menghancurkan token dan mengembalikan nilai logout sama dengen true dengen metode GET", function(done){
+    chai.request(server)
+    .get('/api/users/destroy')
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      res.body.should.be.a('object');
+      res.body.should.have.property('logout');
+      res.body.logout.should.equal(true);
+      done();
+    })
+  })
 })
