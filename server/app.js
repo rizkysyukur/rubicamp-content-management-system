cbcc67736c66549e7b618fa8c561e1598e229253
saefulrizky <saefulrizky2@gmail>
2018-09-04 12:19:17 +0700

    TDD datadate function has added

--- server/app.js
+++ server/app.js
@@ -9,6 +9,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dataRouter = require('./routes/data');
 var datadateRouter = require('./routes/datadate');
+var mapsRouter = require('./routes/maps');
 
 var app = express();
 
@@ -36,6 +37,7 @@ app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/data', dataRouter);
 app.use('/api/datadate', datadateRouter);
+app.use('/api/maps', mapsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
