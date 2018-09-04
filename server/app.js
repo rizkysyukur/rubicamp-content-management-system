4cd3ec4a5d550557deead97f3cd7c22c3a62bd0d
saefulrizky <saefulrizky2@gmail>
2018-09-04 11:10:38 +0700

    BREAD datadate function has added

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const mongoose = require('mongoose');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var dataRouter = require('./routes/data');
+var datadateRouter = require('./routes/datadate');
 
 var app = express();
 
@@ -34,6 +35,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/data', dataRouter);
+app.use('/api/datadate', datadateRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
