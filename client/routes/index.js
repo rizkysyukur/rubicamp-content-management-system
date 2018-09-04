bfcc742248f1613325fe76130ab8d67eb8cb8aca
saefulrizky <saefulrizky2@gmail>
2018-09-04 16:49:54 +0700

    Add interface client (dashboard, login, register, home)

--- client/routes/index.js
+++ client/routes/index.js
@@ -3,7 +3,35 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/login', function(req, res, next) {
+  res.render('login');
+});
+
+router.get('/register', function(req, res, next) {
+  res.render('register');
+});
+
+router.get('/home', function(req, res, next) {
+  res.render('home');
+});
+
+router.get('/data', function(req, res, next) {
+  res.render('home');
+});
+
+router.get('/dataDate', function(req, res, next) {
+  res.render('home');
+});
+
+router.get('/maps', function(req, res, next) {
+  res.render('home');
+});
+
+router.get('/logout', function(req, res, next) {
+  res.render('login');
 });
 
 module.exports = router;
